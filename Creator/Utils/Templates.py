1a89acaabba528856a7ccb4e572e1a896900ab06
Gustavo <58173403+GFrainer@users.noreply.github.com>
2020-10-18 18:23:25 -0300

    Update Templates.py
    Add verification if directory exists

--- Creator/Utils/Templates.py
+++ Creator/Utils/Templates.py
@@ -2,6 +2,8 @@ import os
 
 def getTemplates(dir):
     templates = dict()
+    if not os.path.isdir("../Templates/"+dir+"/"):
+        return templates
     for temp in [temp for temp in os.listdir("../Templates/"+dir+"/") if temp.endswith(".html")]:
         with open("../Templates/"+dir+"/"+temp, 'r') as template:
             templates[temp[0:temp.find('.')]] = template.read()
