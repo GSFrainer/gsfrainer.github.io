7034b6d0a466a4ebd3c205281b6c7c90cf9d8b9b
Gustavo <58173403+GFrainer@users.noreply.github.com>
2020-10-19 15:18:13 -0300

    Update Creator.py
    Removed Activities tab

--- Creator/Creator.py
+++ Creator/Creator.py
@@ -1,6 +1,5 @@
 import json
 from Utils.Thread import Thread
-from Activities import Activities
 from Posts import Posts
 from Index import Index
 from Home import Home
@@ -9,7 +8,6 @@ from About import About
 index = Index()
 home = Home()
 posts = Posts()
-activities = Activities()
 about = About()
 
 data = dict()
@@ -18,18 +16,15 @@ with open('../Data/Profile.json', 'r') as profile:
 
 homeTh = Thread(target=home.createHome, args=(data,))
 postsTh = Thread(target=posts.createPosts, args=(data,))
-activitiesTh = Thread(target=activities.createActivities, args=(data,))
 aboutTh = Thread(target=about.createAbout, args=(data,))
 
 homeTh.start()
 postsTh.start()
-activitiesTh.start()
 aboutTh.start()
 
 pages = dict()
 pages["Home"] = homeTh.join()
 pages["Posts"] = postsTh.join()
-pages["Activities"] = activitiesTh.join()
 pages["About"] = aboutTh.join()
 
 index.createIndex(data, pages)
